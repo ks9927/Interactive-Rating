bccfe5506987bc1d2818f11a5a5eed77fe5e8542
Nova <keamberahsands@gmail.com>
2025-01-20 15:39:12 -0500

    added variables for javascript

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,11 @@
+const ratingScreen = document.querySelector(".rating-container");
+const thanksScreen = document.querySelector(".thanks-container");
+const ratingsBtns = document.querySelectorAll(".rating-btn");
+const  ratingResults = document.querySelector(".rating-result");
+const submitBtn = document.querySelector(".submit-button");
+
+
+
 
 
 
